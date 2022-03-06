16f24052a97f9e96feec75ad0bf6f03f7492aa49
Remison Junio <remisonpires@gmail.com>
2022-03-05 21:15:32 -0300

    Detalhes do Header e início da Seção Home

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const nav = document.querySelector('.nav')
+const nav = document.querySelector('.nav');
 
-const btnHamburger = document.querySelector('#btn-hamburger').addEventListener('click', ()=> {
+const btnHamburger = document.querySelector('#btn-hamburger');
+
+btnHamburger.addEventListener('click', ()=> {
 	nav.classList.toggle('active');
+})
+
+btnHamburger.addEventListener('blur', ()=> {
+	nav.classList.remove('active');
+})
+
+const menuIdioma = document.querySelector('.menu-idioma');
+const btnIdioma = document.querySelector('.btn-idioma');
+
+btnIdioma.addEventListener('click', () => {
+	menuIdioma.classList.toggle('active');
+})
+
+btnIdioma.addEventListener('blur', () => {
+	menuIdioma.classList.remove('active');
 })
